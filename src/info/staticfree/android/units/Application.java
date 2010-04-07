83fb306557745a1b4a2c431f7e070deb91689a4a
Steve Pomeroy <steve@staticfree.info>
2010-04-07 10:52:59 -0400

    Incremental improvement
    Moved Units init code to Application
    
    Added basic history list

---
+++ src/info/staticfree/android/units/Application.java
@@ -0,0 +1,80 @@
+package info.staticfree.android.units;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Vector;
+
+import net.sourceforge.unitsinjava.Env;
+import net.sourceforge.unitsinjava.Tables;
+import android.util.Log;
+
+public class Application extends android.app.Application {
+	private final static String TAG = "units";
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		initUnits();
+	}
+	
+    private void initUnits(){
+        Env.filenames = new Vector<String>();
+        Env.filenames.add("units.dat");
+        
+        Env.locale = Locale.getDefault().toString();
+        Env.quiet = true;
+        Env.oneline = true;
+        
+        
+        Env.out = new Env.Writer(){
+        	@Override
+        	public void print(String s) {
+        		Log.i(TAG, s);
+        		
+        	}
+        	
+        	@Override
+        	public void println(String s) {
+        		Log.i(TAG, s);
+        		
+        	}
+        };
+        
+        Env.err = new Env.Writer() {
+			
+			@Override
+			public void println(String s) {
+				Log.e(TAG, s);
+				
+			}
+			
+			@Override
+			public void print(String s) {
+				Log.e(TAG, s);
+				
+			}
+		};
+        
+        Env.files = new Env.FileAcc() {
+			
+			@Override
+			public BufferedReader open(String name) {
+				try {
+					Log.d("Units", "reading definitions from "+name);
+					final InputStream is = getAssets().open(name);
+					
+					
+					return new BufferedReader(new InputStreamReader(is, "8859_1"), 16000);
+				}catch (final IOException ioe){
+					ioe.printStackTrace();
+				}
+				return null;
+			}
+		};
+		 
+        Tables.build();
+    }
+}
--- src/info/staticfree/android/units/Units.java
+++ src/info/staticfree/android/units/Units.java
@@ -2,37 +2,33 @@ package info.staticfree.android.units;
 
 import info.staticfree.android.units.ValueGui.ConversionException;
 import info.staticfree.android.units.ValueGui.ReciprocalException;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Locale;
-import java.util.Vector;
-
-import net.sourceforge.unitsinjava.Env;
 import net.sourceforge.unitsinjava.EvalError;
-import net.sourceforge.unitsinjava.Tables;
 import net.sourceforge.unitsinjava.Util;
 import net.sourceforge.unitsinjava.Value;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class Units extends Activity implements OnClickListener {
+	private final static String TAG = Units.class.getSimpleName();
 	
 	private EditText wantEditText;
 	private EditText haveEditText;
 	private Button goButton;
-	private TextView result;
-	private TextView resultLog;
+	private TextView resultView;
 	private TextView errorMsgView;
+	private ListView history;
+	private LinearLayout historyDrawer;
+	private Button historyClose;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,66 +39,65 @@ public class Units extends Activity implements OnClickListener {
         wantEditText = ((EditText)findViewById(R.id.want));
         haveEditText = ((EditText)findViewById(R.id.have));
         goButton = ((Button)findViewById(R.id.go));
-        result = ((TextView)findViewById(R.id.answer));
-        resultLog = ((TextView)findViewById(R.id.answer_log));
         errorMsgView = ((TextView)findViewById(R.id.error_msg));
-        
-        Env.filenames = new Vector<String>();
-        Env.filenames.add("units.dat");
-        
-        Env.locale = Locale.getDefault().toString();
-        Env.quiet = true;
-        Env.oneline = true;
-        
-        
-        Env.out = new Env.Writer(){
-        	@Override
-        	public void print(String s) {
-        		result.append(s);
-        		
-        	}
-        	
-        	@Override
-        	public void println(String s) {
-        		result.append(s + "\n");
-        		
-        	}
-        };
-        
-        Env.err = new Env.Writer() {
-			
-			@Override
-			public void println(String s) {
-				result.append(s);
-				
-			}
-			
-			@Override
-			public void print(String s) {
-				result.append(s + "\n");
-				
-			}
-		};
-        
-        Env.files = new Env.FileAcc() {
-			
-			@Override
-			public BufferedReader open(String name) {
-				try {
-					Log.d("Units", "reading definitions from "+name);
-					final InputStream is = getAssets().open(name);
-					
-					return new BufferedReader(new InputStreamReader(is, "8859_1"), 16000);
-				}catch (final IOException ioe){
-					ioe.printStackTrace();
-				}
-				return null;
-			}
-		};
-		
-        Tables.build();
+        resultView = ((TextView)findViewById(R.id.result));
+        history = ((ListView)findViewById(R.id.history_list));
+        historyDrawer = ((LinearLayout)findViewById(R.id.history_drawer));
+        historyClose = ((Button)findViewById(R.id.history_close));
         
 		goButton.setOnClickListener(this);
+		
+		historyAdapter = new ArrayAdapter<HistoryEntry>(this, android.R.layout.simple_list_item_1);
+		history.setAdapter(historyAdapter);
+		setHistoryVisible(false);
+		
+		resultView.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				setHistoryVisible(true);
+				
+			}
+		});
+		historyClose.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+			setHistoryVisible(false);
+				
+			}
+		});
+		
+    }
+   
+    private void setHistoryVisible(boolean visible){
+    	if (visible){
+    		historyDrawer.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.history_show));
+    		historyDrawer.setVisibility(View.VISIBLE);
+    	}else{
+			historyDrawer.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.history_hide));
+			historyDrawer.setVisibility(View.INVISIBLE);
+    	}
+    }
+    
+    private class HistoryEntry {
+    	public String haveExpr;
+    	public String wantExpr;
+    	public double result;
+    	public HistoryEntry(String haveExpr, String wantExpr, double result) {
+    		this.haveExpr = haveExpr;
+    		this.wantExpr = wantExpr;
+    		this.result = result;
+		}
+    	
+    	@Override
+    	public String toString() {
+    		return haveExpr + " = " + Util.shownumber(result) + wantExpr;
+    	}
+    }
+    
+    public void addToHistory(String haveExpr, String wantExpr, double result){
+    	final HistoryEntry histEnt = new HistoryEntry(haveExpr, wantExpr, result);
+    	resultView.setText(histEnt.toString());
+    	historyAdapter.add(histEnt);
     }
     
     public void loadAutocomplete(){
@@ -135,7 +130,8 @@ public class Units extends Activity implements OnClickListener {
     			errorMsgView.setText("reciprocal conversion");
     			resultVal = ValueGui.convertNonInteractive(re.reciprocal, want);
     		}
-    		result.setText(Util.shownumber(resultVal) + " " + wantStr);
+
+    		addToHistory(haveStr, wantStr, resultVal);
 
     	} catch (final EvalError e) {
 
@@ -159,4 +155,5 @@ public class Units extends Activity implements OnClickListener {
     	}	
     }
     
+    private ArrayAdapter historyAdapter;
 }
